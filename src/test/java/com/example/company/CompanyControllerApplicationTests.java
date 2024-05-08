bbab117bc4c71e4c2ba9168f55082d2acf060545
Dragana <draganaa.volas@gmail.com>
2024-05-08 17:55:22 +0200

    Added separate test resources file.

--- src/test/java/com/example/company/CompanyControllerApplicationTests.java
+++ src/test/java/com/example/company/CompanyControllerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.company;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompanyControllerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
