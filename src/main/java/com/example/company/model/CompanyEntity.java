98fc5362611cde45b47256dbe5552a9232be701e
Dragana <draganaa.volas@gmail.com>
2024-05-08 14:32:14 +0200

    Added basic entities for company and employee.

--- src/main/java/com/example/company/model/CompanyEntity.java
+++ src/main/java/com/example/company/model/CompanyEntity.java
@@ -1,4 +1,24 @@
 package com.example.company.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "company")
 public class CompanyEntity {
+
+    @Id
+    private Integer id;
+
+    private String name;
+    private String domain;
+    private String address;
+    private String email;
+
+    @OneToMany(mappedBy = "company")
+    private List<EmployeeEntity> employees;
 }
--- src/main/java/com/example/company/model/EmployeeEntity.java
+++ src/main/java/com/example/company/model/EmployeeEntity.java
@@ -1,4 +1,25 @@
 package com.example.company.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class EmployeeEntity {
+
+    @Id
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String title;
+    private Double salary;
+    private String department;
+
+    @ManyToOne
+    private CompanyEntity company;
 }
